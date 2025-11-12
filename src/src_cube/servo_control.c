d60439e27c6aa999c3a06b243b5be1749f76804d
Hombosh Oleh <oleh.hombosh@gmail.com>
2025-11-12 11:17:43 +0200

    Small refactoring

--- src/src_cube/servo_control.c
+++ src/src_cube/servo_control.c
@@ -5,9 +5,6 @@
 // imports from main.h
 extern PCA9685_HandleTypeDef pca9685;
 
-const uint8_t const NUM_SERVOS = 5;
-static const uint8_t servo_channels[NUM_SERVOS] = { 0, 1, 2, 3, 4 };
-
 void SetServo1Angle(uint8_t angle) {
     PCA9685_SetServoAngle(&pca9685, 0, angle);
 }
